bc57b71c4ac070dbddf5da058179d9e19d3b8837
felipe <felipe.enriquez@gmail.com>
2018-10-25 20:06:19 -0500

    primersaludo

---
+++ solution.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+get '/' do
+    
+    if params["nombre"] == ""
+        @nombre = "desconocido!"
+    elsif params == {}
+        @nombre = "desconocido"
+    else
+        @nombre = params["nombre"]    
+    end   
+    erb :index
+
+end
